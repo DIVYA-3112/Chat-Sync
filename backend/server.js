e7f2cd1d115ebfbfe2cc8acf63287e0be6dc7ec3
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-02-17 17:48:17 +0530

    Update server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const {chats} = require("./data");
 
 dotenv.config();
 
@@ -8,7 +9,16 @@ const app = express();
 const PORT = process.env.PORT || 5001; 
 
 app.get("/", (req, res) => {
-    res.end("Server started !!");
+    res.send("Server started !!");
 });
+
+app.get("/api/chats" , (req,res) => {
+    res.send(chats);
+});
+
+app.get("/api/chat/:id", (req,res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+})
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
